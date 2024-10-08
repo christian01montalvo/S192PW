97f83a264f82a454e06e37a5c93cc75d1dab42e3
christian01montalvo <167219601+christian01montalvo@users.noreply.github.com>
2024-10-08 17:43:05 -0600

    fin practica 3

--- introlaravel/resources/views/cliente.blade.php
+++ introlaravel/resources/views/cliente.blade.php
@@ -1,36 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/js/app.js'])
-    <link rel="stylesheet" href="{{ asset('css/fondo.css') }}">
-
-    <title>Consultar Clientes</title>
-</head>
-<body>
-
-    {{-- Inicia navbar --}}
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('inicio') }}">Turista sin Maps</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ route('formulario') }}">Registro Clientes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Consulta Clientes</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    {{-- Finaliza navbar --}}
-
+@extends('layouts.plantilla')
+@section('titulo', 'formulario')
+@section('contenido')
     {{-- Inicia tarjetaCliente --}}
     <div class="container mt-5 col-md-8">
         <div class="card text-justify font-monospace">
@@ -50,5 +20,4 @@
     </div>
     {{-- Finaliza tarjetaCliente --}}
 
-</body>
-</html>
+@endsection
