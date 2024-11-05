2562aa60117d5f200cd9268a454d509f16de68f3
christian01montalvo <167219601+christian01montalvo@users.noreply.github.com>
2024-11-04 19:51:11 -0600

    examen 2do p

--- repaso2/routes/web.php
+++ repaso2/routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'bienvenido')-> name('bienvenido');
+Route::view('/reg', 'registro')-> name('registro');
 
-
-Route::get('/registro', function () {
-    return view('registro');
-});
